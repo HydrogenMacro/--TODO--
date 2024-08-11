5ed4f98bacc88e82c880ea50fb979e54fb728826
HydrogenMacro <hydrogenmacro@gmail.com>
2024-08-11 16:54:22 -0400

    annoyin bugs

--- src/game_objs/player.rs
+++ src/game_objs/player.rs
@@ -4,33 +4,36 @@ use macroquad::prelude::*;
 use rapier2d::prelude::*;
 use macroquad::experimental::collections::storage;
 use macroquad::prelude::animation::{AnimatedSprite, Animation};
+use rapier2d::control::KinematicCharacterController;
 use crate::assets::Assets;
 use crate::game_objs::Texture;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct Player;
 
-#[derive(Bundle)]
+#[derive(Bundle, Debug)]
 pub struct PlayerBundle {
 	player_marker: Player,
 	texture: Texture2D,
 	rigid_body_handle: RigidBodyHandle,
-	collider_handle: ColliderHandle
+	collider_handle: ColliderHandle,
+	kinematic_character_controller: KinematicCharacterController
 }
 impl Default for PlayerBundle {
 	fn default() -> Self {
 		let mut rigid_body_set = storage::get_mut::<RigidBodySet>();
 		let mut collider_set = storage::get_mut::<ColliderSet>();
-		let rigid_body_handle = rigid_body_set.insert(RigidBodyBuilder::kinematic_position_based());
+		let rigid_body_handle = rigid_body_set.insert(RigidBodyBuilder::dynamic());
 		PlayerBundle {
 			player_marker: Player,
 			texture: storage::get::<Assets>().0.get(&"favicon.png").unwrap().clone(),
 			rigid_body_handle,
 			collider_handle: collider_set.insert_with_parent(
-				ColliderBuilder::cuboid(10., 10.),
+				ColliderBuilder::cuboid(10., 10.).mass(10.),
 				rigid_body_handle,
 				&mut rigid_body_set
 			),
+			kinematic_character_controller: KinematicCharacterController::default()
 		}
 	}
 }
--- src/levels/level1.rs
+++ src/levels/level1.rs
@@ -1,25 +1,29 @@
 use hecs::*;
+use macroquad::experimental::collections::storage;
 use macroquad::prelude::*;
 use rapier2d::prelude::*;
+
 use crate::game_objs::player::PlayerBundle;
 use crate::levels::Level;
 
+#[derive(Debug)]
 pub struct Level1;
 
 impl Level for Level1 {
 	fn init(&self, world: &mut World) {
-		world.spawn(
-			(PlayerBundle::default(), Level1)
+		let player = world.spawn(
+			PlayerBundle::default()
 		);
+		world.insert(player, (Level1, )).unwrap();
 	}
 
 	fn update(&self, world: &mut World, change_level: &mut bool) {
-		let mut q =  world.query::<With<&Texture2D, &Level1>>();
-		let texture = q.iter().next().unwrap().1;
-		draw_texture(texture, 0., 0., WHITE);
+		let mut q = world.query::<With<(&Texture2D, &ColliderHandle), &Level1>>();
+		let (texture, collider_handle) = q.iter().next().unwrap().1;
+		let binding = storage::get::<ColliderSet>();
+		let pos = binding.get(*collider_handle).unwrap().translation();
+		draw_texture(texture, pos.x, pos.y, WHITE);
 	}
 
-	fn cleanup(&self, world: &mut World) {
-
-	}
+	fn cleanup(&self, world: &mut World) {}
 }
